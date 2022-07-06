9ee1d31835671a8ed0f6c5bf413e9c1a05fc1bb3
Anslem1 <andrewanslem1@gmail.com>
2022-07-06 23:09:02 +0100

    Rock-Paper-scissors

---
+++ app.js
@@ -0,0 +1,15 @@
+const computerPlay = () =>  {
+    const rockPaperSci = ["Rock", "Paper", "Scissors"]
+const out = []
+const elements = 1
+
+    for (let i = 0; i < elements; i++) {
+        out.push(...rockPaperSci.splice(Math.floor(Math.random() * rockPaperSci.length),1));
+    }
+return out.toString()
+}
+
+computerPlay()
+
+
+
