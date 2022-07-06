766c3bcb7a91a9ab3e09bfae111b7c8096b231bd
Anslem1 <andrewanslem1@gmail.com>
2022-07-06 23:17:23 +0100

    BLue

--- app.js
+++ app.js
@@ -6,10 +6,15 @@ const elements = 1
     for (let i = 0; i < elements; i++) {
         out.push(...rockPaperSci.splice(Math.floor(Math.random() * rockPaperSci.length),1));
     }
-return out.toString()
+ console.log(out.toString())
 }
 
 computerPlay()
+
+const playRounds = (playerSelection, computerSelection) => {
+
+}
+
 
 
 
