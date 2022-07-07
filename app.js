5074723bc924e38c61692955f3c204a7a84c2e02
Anslem1 <andrewanslem1@gmail.com>
2022-07-07 12:29:29 +0100

    master

--- app.js
+++ app.js
@@ -1,20 +1,56 @@
-const computerPlay = () =>  {
-    const rockPaperSci = ["Rock", "Paper", "Scissors"]
-const out = []
-const elements = 1
+// generate rock, paper scissors at random..
+const computerPlay = () => {
+    const rockPaperSci = ['rock', 'paper', 'scissors'];
+    const out = [];
+    const elements = 1;
 
     for (let i = 0; i < elements; i++) {
-        out.push(...rockPaperSci.splice(Math.floor(Math.random() * rockPaperSci.length),1));
+        out.push(
+            ...rockPaperSci.splice(Math.floor(Math.random() * rockPaperSci.length), 1)
+        );
     }
- console.log(out.toString())
+    return out.toString().toLowerCase();
 }
 
-computerPlay()
+//A round of Rock, Paper, Scissors to determine a winner
 
-const playRounds = (playerSelection, computerSelection) => {
+const playRound = (computerSelection, playerSelection) => {
+  if (computerSelection === 'rock' && playerSelection === 'scissors') {
+    return 'You lose! Rock beats scissors'
+  } else if (computerSelection === 'paper' && playerSelection === 'rock') {
+    return 'You lose! Paper beats Rock'
+  } else if (computerSelection === 'scissors' && playerSelection === 'paper') {
+      return 'You lose scissors beats paper'
+    
+  } else if (playerSelection === computerSelection) {
+      return `Its a tie, you both picked ${computerSelection}`
+  }
+  else {
+    return `You win! ${playerSelection} beats ${computerSelection}`
+  }
+}
+
+const computerSelection = computerPlay()
+
+const playerSelection = prompt(
+  'Enter, Rock, Paper or scissors'
+).toLocaleLowerCase()
+
+if (
+  (playerSelection !== 'rock',
+  playerSelection !== 'paper',
+  playerSelection !== 'scissors')
+) {
+    alert('Please enter Rock, Paper or Scissors')
+    for (let i = 0; i < playerSelection.length; i++){
+        playerSelection = prompt('Enter, Rock, Paper or scissors').toLocaleLowerCase()
+
+    }
+  
+
+
 
 }
 
-
-
+playRound(computerSelection, playerSelection)
 
