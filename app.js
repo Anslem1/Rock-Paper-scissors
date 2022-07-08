64f7a2fdd653c32ce3268ebe1850f599f77a48e2
Anslem1 <andrewanslem1@gmail.com>
2022-07-08 14:21:28 +0100

    5 Rounds

--- app.js
+++ app.js
@@ -1,56 +1,55 @@
 // generate rock, paper scissors at random..
-const computerPlay = () => {
-    const rockPaperSci = ['rock', 'paper', 'scissors'];
-    const out = [];
-    const elements = 1;
+let playerScore = 0
+let compScore = 0
 
-    for (let i = 0; i < elements; i++) {
-        out.push(
-            ...rockPaperSci.splice(Math.floor(Math.random() * rockPaperSci.length), 1)
-        );
-    }
-    return out.toString().toLowerCase();
+const computerPlay = () => {
+  const rockPaperSci = ['rock', 'paper', 'scissors']
+  const out = []
+  const elements = 1
+
+  for (let i = 0; i < elements; i++) {
+    out.push(
+      ...rockPaperSci.splice(Math.floor(Math.random() * rockPaperSci.length), 1)
+    )
+  }
+  return out.toString().toLowerCase()
 }
 
 //A round of Rock, Paper, Scissors to determine a winner
 
 const playRound = (computerSelection, playerSelection) => {
   if (computerSelection === 'rock' && playerSelection === 'scissors') {
+    compScore++
     return 'You lose! Rock beats scissors'
   } else if (computerSelection === 'paper' && playerSelection === 'rock') {
+    compScore++
     return 'You lose! Paper beats Rock'
   } else if (computerSelection === 'scissors' && playerSelection === 'paper') {
-      return 'You lose scissors beats paper'
-    
+    compScore++
+    return 'You lose scissors beats paper'
   } else if (playerSelection === computerSelection) {
-      return `Its a tie, you both picked ${computerSelection}`
-  }
-  else {
-    return `You win! ${playerSelection} beats ${computerSelection}`
+    return `Its a tie, you both picked ${computerSelection}`
+  } else {
+    playerScore++
+    return `You won! ${playerSelection} beats ${computerSelection}`
   }
 }
 
-const computerSelection = computerPlay()
+//five rounds of Rock, Paper, Scissors to determine a winner
 
 const playerSelection = prompt(
   'Enter, Rock, Paper or scissors'
 ).toLocaleLowerCase()
 
-if (
-  (playerSelection !== 'rock',
-  playerSelection !== 'paper',
-  playerSelection !== 'scissors')
-) {
-    alert('Please enter Rock, Paper or Scissors')
-    for (let i = 0; i < playerSelection.length; i++){
-        playerSelection = prompt('Enter, Rock, Paper or scissors').toLocaleLowerCase()
-
-    }
-  
-
-
-
+const game = () => {
+  for (let i = 0; i < 5; i++) {
+    const computerSelection = computerPlay()
+    console.log(playRound(computerSelection, playerSelection))
+  }
+  if (playerScore > compScore) {
+    console.log('You win!')
+  } else if (compScore > playerScore) {
+    console.log('You lose')
+  }
 }
-
-playRound(computerSelection, playerSelection)
-
+game()
